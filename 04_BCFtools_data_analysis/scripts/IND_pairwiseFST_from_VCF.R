4ceb1f482ed4f29fb8ab51169f2db9cd6314f584
akamolphat <a.kamolphat@gmail.com>
2023-07-07 20:37:41 +1200

    Updated to use TableS1.2v2 instead.

--- 04_BCFtools_data_analysis/scripts/IND_pairwiseFST_from_VCF.R
+++ 04_BCFtools_data_analysis/scripts/IND_pairwiseFST_from_VCF.R
@@ -6,7 +6,7 @@
 # metadtfile <- args[2]
 # outputxlsx <- args[3]
 vcffile <- "../01_download_data/BCFtools_IND/variant_calls.IND.bialminQ30minGQ30DP15-100.norep.highnegfis.lmiss20.nosingledoubletons.vcfthin.hwe.snps.vcf.gz"
-metadtfile <- "../01_download_data/TableS1.2.csv"
+metadtfile <- "../01_download_data/TableS1.2v2.csv"
 outputxlsx <- "results/FST/02_IND/pairwiseFST_IND_BCF_hwe_100bs.xlsx"
 
 # Create output folder to store outputs from HWE filters
@@ -34,9 +34,9 @@ gl@other$ind.metrics <- metadtsub
 # Assign individual ID to individual metadata
 gl@other$ind.metrics$ID <- indNames(gl)
 # Define Pop4
-gl$other$ind.metrics$IND_pop_recode <- gl$other$ind.metrics$popdef2
+gl$other$ind.metrics$IND_pop_recode <- gl$other$ind.metrics$popdef1
 gl$other$ind.metrics <- gl$other$ind.metrics %>%
-  mutate(IND_pop_recode = replace(IND_pop_recode, ID %in% paste("M0", seq(210, 215), sep = ""), "Maharashtra subpop. A"))
+  mutate(IND_pop_recode = replace(IND_pop_recode, popdef1 %in% "Maharashtra subpopulation A", "Maharashtra subpop. A"))
 # pop(gl) <- gl$other$ind.metrics$IND_pop_recode
 pop(gl) <- gl@other$ind.metrics$IND_pop_recode
 
@@ -51,10 +51,10 @@ reichfstmat$bootstraps$p_value <- rowSums(reichfstmat$bootstraps[,6:bootcol]<0)/
 reichpvalmat <- pivot_wider(reichfstmat$bootstraps[,c("pop1", "pop2", "p_value")], names_from = pop2, values_from = p_value)
 
 # Attach population information to genlight object
-pop(gl) <- gl$other$ind.metrics$popdef2
-# FST between all popdef2 ----------------------------------
+pop(gl) <- gl$other$ind.metrics$popdef1
+# FST between all popdef1 ----------------------------------
 fstmat2 <- gl.fst.pop(gl, nboots = bootn)
-# Reich FST between all popdef2 ----------------------------
+# Reich FST between all popdef1 ----------------------------
 reichfstmat2 <- reich.fst(gl, bootstrap=bootn)
 bootcol <- 6+bootn-1 
 reichfstmat2$bootstraps$p_value <- rowSums(reichfstmat2$bootstraps[,6:bootcol]<0)/bootn 
@@ -69,10 +69,10 @@ write.xlsx(fstmat$Bootstraps, file = outputxlsx, sheetName = "FST_100bs_val", ap
 write.xlsx(reichfstmat$fsts, file = outputxlsx, sheetName = "ReichFST_100bs", append = T)
 write.xlsx(reichpvalmat, file = outputxlsx, sheetName = "Reichpval_100bs", append = T)
 write.xlsx(reichfstmat$bootstraps, file = outputxlsx, sheetName = "ReichFST_100bs_val", append = T)
-# Add spreadsheet with popdef2 matrix instead of pop_INDrecode
-write.xlsx(fstmat2$Fsts, file = outputxlsx, sheetName = "FST_100bs_popdef2", append = T)
-write.xlsx(fstmat2$Pvalues, file = outputxlsx, sheetName = "pval_100bs_popdef2", append = T)
-write.xlsx(fstmat2$Bootstraps, file = outputxlsx, sheetName = "FST_100bs_val_popdef2", append = T)
-write.xlsx(reichfstmat2$fsts, file = outputxlsx, sheetName = "ReichFST_100bs_popdef2", append = T)
-write.xlsx(reichpvalmat2, file = outputxlsx, sheetName = "Reichpval_100bs_popdef2", append = T)
-write.xlsx(reichfstmat2$bootstraps, file = outputxlsx, sheetName = "ReichFST_100bs_val_popdef2", append = T)
+# Add spreadsheet with popdef1 matrix instead of pop_INDrecode
+write.xlsx(fstmat2$Fsts, file = outputxlsx, sheetName = "FST_100bs_popdef1", append = T)
+write.xlsx(fstmat2$Pvalues, file = outputxlsx, sheetName = "pval_100bs_popdef1", append = T)
+write.xlsx(fstmat2$Bootstraps, file = outputxlsx, sheetName = "FST_100bs_val_popdef1", append = T)
+write.xlsx(reichfstmat2$fsts, file = outputxlsx, sheetName = "ReichFST_100bs_popdef1", append = T)
+write.xlsx(reichpvalmat2, file = outputxlsx, sheetName = "Reichpval_100bs_popdef1", append = T)
+write.xlsx(reichfstmat2$bootstraps, file = outputxlsx, sheetName = "ReichFST_100bs_val_popdef1", append = T)
